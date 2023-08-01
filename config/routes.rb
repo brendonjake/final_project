d52f09c5b72edd422dde304418f5505df57cf4bf
brendonjake <macapaz.b1998@gmail.com>
2023-08-01 17:34:24 +0800

    [Feature2] Added admin and client constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  constraints(ClientDomainConstraint.new) do
+    root 'client/home#index', as: 'client_root'
+    devise_for :users, controllers: {
+      sessions: 'client/sessions'
+    }, as: 'client'
+    # namespace :client do
+    # end
+  end
+
+  constraints(AdminDomainConstraint.new) do
+    root 'admin/home#index', as: 'admin_root'
+    devise_for :users, controllers: {
+      sessions: 'admin/sessions'
+    }, as: 'admin'
+    # namespace :admin do
+    # end
+  end
 end
