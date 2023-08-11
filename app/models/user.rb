7a91f6b5f0841a03c2503cf585a3b6a55e712cd4
brendonjake <macapaz.b1998@gmail.com>
2023-08-11 19:52:54 +0800

    [Feature 8] user increment children members

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   enum role: { client: 0, admin: 1 }
 
-  belongs_to :parent, class_name: 'User', optional: true
+  belongs_to :parent, class_name: 'User', optional: true, counter_cache: :children_member
   has_many :children, class_name: 'User', foreign_key: 'parent_id'
 
   validates :phone, phone: {
