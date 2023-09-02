f519641b22fb125c3b93903ae26551acc5d03f74
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-02 18:42:42 +0800

    [Feature 28] index for users

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,7 @@ Rails.application.initialize!
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.irregular 'bonus', 'bonuses'
+end
